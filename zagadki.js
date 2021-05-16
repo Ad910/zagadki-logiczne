6725a479b0b3a6c8383d4b85f8b6d076ad74c0bd
Ad910 <66313269+Ad910@users.noreply.github.com>
2021-05-17 01:25:40 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,57 @@
+function z1()
+{
+     document.getElementById("z1").innerHTML="Litera "A";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Tu znajdziesz odpowiedź";
+    } 
+
+
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="Mały, czerwony trójkąt";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Tu znajdziesz odpowiedź";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="W słowniku";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="Nie! Przecież nie umie mówić.";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Ołówek leżał przy ścianie
+
+";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Tu znajdziesz odpowiedź";
+    }
